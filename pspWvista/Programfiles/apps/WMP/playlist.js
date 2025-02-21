457475662aa8bea532d3ab7f978c4c3f7e84a000
Old Web Portals 4 PSP <76764433+owp4psp@users.noreply.github.com>
2025-02-21 01:09:56 +0000

    Vista #5
    5

---
+++ pspWvista/Programfiles/apps/WMP/playlist.js
@@ -0,0 +1,12 @@
+//Song List - Fill out playlist info below...
+
+var i = 0
+var songList = new Array;
+function Song(songData){
+	songList[i]=songData;
+	i++
+}
+
+//BEGIN LIST
+Song("Test 1||Unknown||file:/psp/common/pspWvista/programfiles/apps/WMP/music/2.mp3");
+// END LIST
